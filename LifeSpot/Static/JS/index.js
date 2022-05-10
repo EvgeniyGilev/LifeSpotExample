847083c2a673003272586e20fa62dfa9c187b196
Евгений Гилев <euggil@yandex.ru>
2022-05-10 21:38:40 +0500

    проверяем замыкание

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -30,12 +30,11 @@ function handleSession() {
 }
 
 // Функция для фильтрации контента
-function filterContent(userInput) {
-    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+function filterContent() {
     let elements = document.getElementsByClassName('video-container');
     for (let i = 0; i < elements.length; i++) {
         let videoText = elements[i].querySelector('.video-title').innerText;
-        if (!videoText.toLowerCase().includes(userInput)) {
+        if (!videoText.toLowerCase().includes(inputParseFunction())) {
             elements[i].style.display = 'none';
         } else {
             elements[i].style.display = 'inline-block';
