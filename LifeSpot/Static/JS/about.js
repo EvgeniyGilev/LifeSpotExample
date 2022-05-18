15e297f5d54d22d3dac4142cb1c8995b19812644
Евгений Гилев <euggil@yandex.ru>
2022-05-18 18:19:57 +0500

    Добавил слайдер

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,61 +1,49 @@
-﻿/*
-* Оставить комментарий
-*
-* */
-function getComment() {
-   // Создаем объект обычного комментария
-   let comment = {}
-  
-   // Запросим имя
-   comment.author = prompt("Как вас зовут ?")
-   if(comment.author== null){
-       return
-   }
-  
-   // Запросим текст
-   comment.text = prompt("Оставьте отзыв")
-   if(comment.text == null){
-       return
-   }
- 
-   // Сохраним текущее время
-   comment.date = new Date().toLocaleString()
-  
-   // Запросим,, хочет ли пользователь оставить полноценный отзыв или это будет обычный комментарий
-   let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
-  
-   if(enableLikes){
-       // Создадим для отзыва новый объект из прототипа - комментария
-       let review = Object.create(comment)
-       // и добавим ему нужное свойство
-       review.rate = 0;
- 
-       // Добавляем отзыв с возможностью пользовательских оценок
-       writeReview(review)
-   } else{
-       // Добавим простой комментарий без возможности оценки
-       writeReview(comment)
-   }
-}
- 
+﻿ 
 /*
-* Запишем объект на страницу
-*
-* */
+ * Запишем объект на страницу
+ *
+ * */
 const writeReview = review => {
     let likeCounter = '';
 
-    // Для проверки, является ли объект отзывом, используем свойство hasOwnProperty
+    // Если публикуется отзыв - добавляем ему кнопку с лайками.
     if (review.hasOwnProperty('rate')) {
-        likeCounter += '           <b style="color: chocolate">Рейтинг:</b>   ' + review.rate;
-    }
 
-    // Запишем результат
-    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}</i></p>` +
-        `<p>${review['text']}</p>` +
-        '</div>';
+        // Генерим идентификатор комментария.
+        let commentId = Math.random();
+        // Для кнопки лайков добавляем: идентификатор, атрибут onclick для передачи идентификатора в функцию, значок лайка, и само значение счётчика отделяем пробелом
+        // Также мы добавили стиль, чтобы кнопка смотрелась лучше и не имела рамок
+        likeCounter += '<button id="' + commentId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate}</button>`
+    }
+    // Запишем результат 
+    document.getElementsByClassName('reviews')[0].innerHTML += ' <div class="review-    text">\n' + `<p> <i> <b>${review['author']}</b> ${review['date']}${likeCounter}</i></p>` + `<p>${review['text']}</p>` + '</div>';
 }
+
+/*
+* Увеличивает счётчик лайков
+*
+* */
+function addLike(id) {
+    // Найдём нужный элемент по id
+    let element = document.getElementById(id);
+
+    // Преобразуем текст элемента в массив, разбив его по пробелам (так как счётчик лайков у нас отделен от символа ❤️пробелом)
+    let array = element.innerText.split(' ')
+
+    // Вытащим искомое значение счётчика и сразу же преобразуем его в число, так как
+    // при сложении любого значения со строкой в JS будет строка, а нам этого не требуется
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    // Увеличим счётчик
+    resultNum += 1
+
+    // Сохраним измененное значение обратно в массив
+    array[array.length - 1] = `${resultNum}`
+
+    // Обновим текст элемента
+    element.innerText = array.join(' ')
+}
+
 
 /*
 * Конструктор, через который создаётся комментарий
@@ -107,4 +95,44 @@ function addComment() {
         // Добавим простой комментарий без возможности оценки
         writeReview(comment)
     }
+}
+
+
+//Слайды
+let slideIndex = 1;
+showSlides(slideIndex);
+/*
+//перетасиквание мышкой
+document.getElementById("mySlide1").onmousemove = function (event){
+    let x = event.offsetX;
+    console.log(x);
+    document.getElementById("mySlide2").style.width = x + "px"
+}
+*/
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
